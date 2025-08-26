5de6e4c041c7ee116488b72a440f486fab532ee5
Gabriel Zini Turri <gabrielturrri@gmail.com>
2025-08-26 16:56:28 -0300

    Update

--- javaScript/apiV12.js
+++ javaScript/apiV12.js
@@ -41,6 +41,7 @@ async function buscarLancamentos(filtros) {
             },
             body: JSON.stringify(filtros),
         });
+        console.log("Resposta da API (raw):", response);
         if (!response.ok) {
             const errorText = await response.text();
             throw new Error(`Falha ao buscar lançamentos: ${response.status} ${response.statusText} - ${errorText}`);
