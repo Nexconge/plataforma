d883ccef59ef9d94ae81a88dcbcbcf781c7cb88f
Nexconge <nexcon@nexconge.com.br>
2025-09-03 15:48:54 -0300

    update

--- javaScript/projects/ScriptPropostaV44.js
+++ javaScript/projects/ScriptPropostaV44.js
@@ -107,7 +107,9 @@ export async function abrirEPreencherModalProposta(mapaManager) {
         document.getElementById('propLoteArea').textContent = (loteSelecionado.Área || '0').toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
         document.getElementById('propLoteValor').textContent = formatadorDeMoeda.format(loteSelecionado.Valor) || 0;
         
-        document.getElementById('propClienteNome').value = "Larissa Zini Turri";
+        document.getElementById('propClienteNome').value = "R$ 250.000,00";
+        document.getElementById('propClienteNome').disabled = true;
+        
 
         // Passo D: Preenche os dados da condição financeira.
         const finValorEntrada = loteSelecionado.Valor * 0.25;
--- javaScript/projects/mainV44.js
+++ javaScript/projects/mainV44.js
@@ -1,6 +1,6 @@
 // Importa as funções públicas dos outros módulos
 import { iniciarMapa } from './ScriptLotesV03.js';
-import { abrirEPreencherModalProposta } from './ScriptPropostaV43.js';
+import { abrirEPreencherModalProposta } from './ScriptPropostaV44.js';
 
 // Função principal de inicialização do aplicativo
 function inicializarApp(empreendimentosJSON, projectsUrl) {
