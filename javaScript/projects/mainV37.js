54eaad672c4e4f003a810fdb38daff3cd3d7b212
Gabriel Zini Turri <gabrielturrri@gmail.com>
2025-10-06 13:56:25 -0300

    Update

--- javaScript/projects/mainV37.js
+++ javaScript/projects/mainV37.js
@@ -1,5 +1,5 @@
 // Importa as funções públicas dos outros módulos
-import { iniciarMapa } from './ScriptLotesV32.js';
+import { iniciarMapa } from './ScriptLotesV33.js';
 import { abrirEPreencherModalProposta } from './ScriptPropostaV01.js';
 
 // Função principal de inicialização do aplicativo
