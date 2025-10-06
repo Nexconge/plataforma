42d177e6fe8cc4d84df31edec9ac14ad4e5074f8
Gabriel Zini Turri <gabrielturrri@gmail.com>
2025-10-06 14:36:36 -0300

    Update Projects

--- javaScript/projects/ScriptLotesV42.js
+++ javaScript/projects/ScriptLotesV42.js
@@ -141,7 +141,7 @@ class MapaLotesManager {
                     permanent: true,
                     direction: "bottom",
                     className: "quadra-tooltip",
-                    offset: [-4, 2] //remove deslocamento padrão
+                    offset: [-6, 1.5] //remove deslocamento padrão
                 });
 
                 marker.loteData = lote;
