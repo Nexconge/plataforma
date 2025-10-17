ac7dd93e82e6c778b2ee958b2296fe6bdbbbffe1
Nexconge <nexcon@nexconge.com.br>
2025-10-17 19:25:47 -0300

    update promissoria

--- javaScript/configurations/notaPromissoriaV33.js
+++ javaScript/configurations/notaPromissoriaV33.js
@@ -258,8 +258,9 @@ window.gerarNotaPromissoria = async function () {
         doc.text(dados.nomeAssinanteAdc, (startX + ((endX - startX) / 2)), yAtual, { align: "center" }); yAtual += 5;
         doc.text(dados.cpfCnpjAssinanteAdc, (startX + ((endX - startX) / 2)), yAtual, { align: "center" }); yAtual += 5;
         doc.text("Assinante Adicional", (startX + ((endX - startX) / 2)), yAtual, { align: "center" }); yAtual += 5;
-        yAtual += 20;
     }
+
+    yAtual += 20;
 
     // Assinatura Avalistas
     if (dados.nomeAvalista_1 !== "" && dados.cpfCnpjAvalista_1 !== "") {
@@ -285,8 +286,9 @@ window.gerarNotaPromissoria = async function () {
         doc.text("Garantidor", (startX + ((endX - startX) / 2)), yAtual, { align: "center" }); yAtual += 5;
     }
 
+    yAtual += 20;
+
     if (dados.nomeAvalista_3 !== "" && dados.cpfCnpjAvalista_3 !== "") {
-        yAtual += 20;
         startX = margemLateral;
         endX = margemLateral + 85;
         doc.line(startX, yAtual, endX, yAtual);
@@ -295,10 +297,10 @@ window.gerarNotaPromissoria = async function () {
         doc.text(dados.nomeAvalista_3, (margemLateral + ((endX - margemLateral) / 2)), yAtual, { align: "center" }); yAtual += 5;
         doc.text(dados.cpfCnpjAvalista_3, (margemLateral + ((endX - margemLateral) / 2)), yAtual, { align: "center" }); yAtual += 5;
         doc.text("Garantidor", (margemLateral + ((endX - margemLateral) / 2)), yAtual, { align: "center" }); yAtual += 5;
+        yAtual -= 20;
     }
 
     if (dados.nomeAvalista_4 !== "" && dados.cpfCnpjAvalista_4 !== "") {
-        yAtual -= 20;
         startX = larguraPagina - margemLateral - 85;
         endX = larguraPagina - margemLateral;
         doc.line(startX, yAtual, endX, yAtual);
