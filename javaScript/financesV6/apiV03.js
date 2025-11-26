945fd46f902f2cdf3716eda7768f0f3525ecac9a
Gabriel Zini Turri <gabrielturrri@gmail.com>
2025-11-26 11:09:26 -0300

    Update

--- javaScript/financesV6/apiV03.js
+++ javaScript/financesV6/apiV03.js
@@ -33,7 +33,7 @@ async function buscarTitulos(filtros) {
     } else {
         baseURL += "version-live";
     }
-    const API_URL = `${baseURL}/api/1.1/wf/buscarmovimentos`;
+    const API_URL = `${baseURL}/api/1.1/wf/buscarmovimentos/initialize`;
 
     // O payload aceita:
     // anos: ["2024"] (Para realizado)
