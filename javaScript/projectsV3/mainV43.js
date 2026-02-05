dd8ba2c16997978f1246db817bee78e102fb94dd
GabrielZT <58994607+GabrielZT@users.noreply.github.com>
2026-02-05 15:00:41 -0300

    Corrigido 'furos' ao renderizar lotes no mapa;

--- javaScript/projectsV3/mainV43.js
+++ javaScript/projectsV3/mainV43.js
@@ -1,4 +1,4 @@
-import { iniciarMapa } from './ScriptLotesV41.js';
+import { iniciarMapa } from './ScriptLotesV42.js';
 import { abrirEPreencherModalProposta } from './ScriptPropostaV09.js';
 
 function inicializarApp(empreendimentosJSON, projectsUrl, username) {
