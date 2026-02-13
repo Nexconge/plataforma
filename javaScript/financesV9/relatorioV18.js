5883ff024ac4012e3a33445db1b1e2aa7d73c699
GabrielZT <58994607+GabrielZT@users.noreply.github.com>
2026-02-13 10:33:58 -0300

    Fix

--- javaScript/financesV9/relatorioV18.js
+++ javaScript/financesV9/relatorioV18.js
@@ -1,5 +1,5 @@
 import { buscarTitulos } from './apiV04.js';
-import { extrairDadosDosTitulos, extrairLancamentosSimples } from './processingV08.js';
+import { extrairDadosDosTitulos, extrairLancamentosSimples } from './processingV09.js';
 
 /**
  * Realiza a busca por ano, processa imediatamente e dispara a geração do Excel.
