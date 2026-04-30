892613f77e484966005911fe8103d75beeb4e762
GabrielZT <58994607+GabrielZT@users.noreply.github.com>
2026-04-30 17:51:54 -0300

    Update

--- javaScript/projectsV6/mainV46.js
+++ javaScript/projectsV6/mainV46.js
@@ -1,4 +1,4 @@
-import { iniciarMapa } from './ScriptLotesV39.js';
+import { iniciarMapa } from './ScriptLotesV40.js';
 import { abrirEPreencherModalProposta } from './ScriptPropostaV03.js';
 
 function inicializarApp(empreendimentosJSON, projectsUrl, username, userModifyers) {
