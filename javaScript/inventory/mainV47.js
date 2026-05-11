26f2aa5c8ffa002577e57e55f8cdee454a16b35b
GabrielZT <58994607+GabrielZT@users.noreply.github.com>
2026-05-11 09:00:10 -0300

    Fix

--- javaScript/inventory/mainV47.js
+++ javaScript/inventory/mainV47.js
@@ -27,7 +27,6 @@ window.filtrarVendasPorMes = function(mes) {
 
 window.iniciarAplicacao = function (textoCadastros) {
     console.log("Iniciando aplicação...");
-    console.log("Texto bruto recebido para cadastros:", textoCadastros);
     try {
         let jsonString = textoCadastros.trim();
         if (!jsonString.startsWith("[")) jsonString = `[${jsonString}]`;
